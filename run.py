7a42ec62c2d3473001cc4f6f8029a6e60b1860d6
tonyzhang25 <tonyzhang@caltech.edu>
2018-10-02 13:10:05 -0700

    Implemented MC

--- run.py
+++ run.py
@@ -21,22 +21,6 @@ agents = [
         'learn model': False,
         'discount rate': 0.9
     },
-    # {
-    #     'learning rate': 0.1,
-    #     'value update': 'TD',
-    #     'lambda': 0,
-    #     'exploration policy': 'softmax',
-    #     'learn model': True,
-    #     'planning steps': 5
-    # },
-    # {
-    #     'learning rate': 0.1,
-    #     'value update': 'TD',
-    #     'lambda': 0,
-    #     'exploration policy': 'e-greedy',
-    #     'learn model': True,
-    #     'planning steps': 5
-    # }
 ]
 
 # Environment spec variable
